5020f575942541cbcf642bc00a837dd94e69efdd
SevenSnowy <zeus0872yt@gmail.com>
2026-09-01 19:31:09 -0300

    Update exercicio 4.rs

--- Relatorio 3/exercicio 4.rs
+++ Relatorio 3/exercicio 4.rs
@@ -3,32 +3,31 @@ use std::io;
 fn calcular_pontuacao(prova1: f64, prova2: f64, redacao: f64) -> f64 {
     let npt = (prova1 + prova2) / 2.0;
     let pf = (npt * 0.6) + (redacao * 0.4);
-
+    
     if pf >= 60.0 {
         println!("Parabens! Candidato aprovado no processo seletivo.");
     } else {
         println!("Infelizmente o candidato nao atingiu a pontuacao minima de aprovacao.");
     }
-
+    
     pf
 }
 
 fn main() {
-    let mut entrada = String::new();
-
+    let mut entrada1 = String::new();
     println!("Digite a nota da Prova Teorica 1:");
-    io::stdin().read_line(&mut entrada).expect("Erro ao ler");
-    let prova1: f64 = entrada.trim().parse().unwrap_or(0.0);
+    io::stdin().read_line(&mut entrada1).expect("Erro ao ler");
+    let prova1: f64 = entrada1.trim().parse().unwrap_or(0.0);
 
-    entrada.clear();
+    let mut entrada2 = String::new();
     println!("Digite a nota da Prova Teorica 2:");
-    io::stdin().read_line(&mut entrada).expect("Erro ao ler");
-    let prova2: f64 = entrada.trim().parse().unwrap_or(0.0);
+    io::stdin().read_line(&mut entrada2).expect("Erro ao ler");
+    let prova2: f64 = entrada2.trim().parse().unwrap_or(0.0);
 
-    entrada.clear();
+    let mut entrada3 = String::new();
     println!("Digite a nota da Redacao:");
-    io::stdin().read_line(&mut entrada).expect("Erro ao ler");
-    let redacao: f64 = entrada.trim().parse().unwrap_or(0.0);
+    io::stdin().read_line(&mut entrada3).expect("Erro ao ler");
+    let redacao: f64 = entrada3.trim().parse().unwrap_or(0.0);
 
     let pontuacao_final = calcular_pontuacao(prova1, prova2, redacao);
     println!("Pontuacao Final: {:.2}", pontuacao_final);
